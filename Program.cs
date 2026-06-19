561f24a92e8e11b0ff5af7641beb1b7d799b17d9
Maxim Churzin <mchurzin@outlook.com>
2026-06-19 15:14:32 -0400

    Edit dbContext to dbContextFactory

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
-builder.Services.AddDbContext<AppDbContext>(options =>
+builder.Services.AddDbContextFactory<AppDbContext>(options =>
     options.UseSqlite("Data Source=invoices.db")
 );
 
