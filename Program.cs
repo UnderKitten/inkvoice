c67f389a4b9345c909664c2b0710e43b07922f80
Maxim Churzin <mchurzin@outlook.com>
2026-06-26 18:23:41 -0400

    Added endpoint for QuestPDF

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 using Inkvoice.Components;
 using Inkvoice.Data;
+using Inkvoice.Models;
+using Inkvoice.Services;
 using Microsoft.EntityFrameworkCore;
+using QuestPDF.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+QuestPDF.Settings.License = LicenseType.Community;
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 builder.Services.AddDbContextFactory<AppDbContext>(options =>
     options.UseSqlite("Data Source=invoices.db")
@@ -26,5 +30,25 @@ app.UseAntiforgery();
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+app.MapGet(
+    "/documents/{id:int}/pdf",
+    async (int id, IDbContextFactory<AppDbContext> factory) =>
+    {
+        using var db = await factory.CreateDbContextAsync();
+        var doc = await db
+            .Documents.Include(d => d.Items)
+            .Include(d => d.Client)
+            .FirstOrDefaultAsync(d => d.Id == id);
+
+        if (doc is null)
+            return Results.NotFound();
+
+        var biz = await db.Businesses.FirstOrDefaultAsync() ?? new Business();
+        var bytes = InvoicePdf.Generate(doc, biz);
+        var fileName = $"{doc.Type}_{doc.Number}.pdf".Replace(" ", "_");
+        return Results.File(bytes, "application/pdf", fileName);
+    }
+);
 
 app.Run();
